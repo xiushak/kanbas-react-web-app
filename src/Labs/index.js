01d1f9276a7dc9d35e8bd204d43b4aaad5b469f9
xiushak <30637265+xiushak@users.noreply.github.com>
2023-10-06 03:05:50 -0400

    a3 completed lab

---
+++ src/Labs/index.js
@@ -0,0 +1,18 @@
+import Assignment3 from "./a3";
+import Assignment4 from "./a4";
+import Assignment5 from "./a5";
+import { Link } from "react-router-dom";
+import Nav from "../Nav";
+
+function Labs() {
+  return (
+    <div className="container">
+      <Nav />
+      <Assignment3 />
+      <Assignment4 />
+      <Assignment5 />
+    </div>
+  );
+}
+
+export default Labs;
